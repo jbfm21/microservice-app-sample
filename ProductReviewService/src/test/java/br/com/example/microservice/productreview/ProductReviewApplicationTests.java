d7850255773f05b8661212ba8703b0c93839f147
Julio <jbfm21@gmail.com>
2021-10-11 00:10:01 -0300

    preparativo para k8s

--- ProductReviewService/src/test/java/br/com/example/microservice/productreview/ProductReviewApplicationTests.java
+++ ProductReviewService/src/test/java/br/com/example/microservice/productreview/ProductReviewApplicationTests.java
@@ -1,13 +1,9 @@
 package br.com.example.microservice.productreview;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductReviewApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
